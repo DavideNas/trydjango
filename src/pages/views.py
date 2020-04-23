023d21a11b66ee0e4b644a4daf3672cdb0931ef5
DavideNas <d.naselli@gmail.com>
2020-04-23 18:02:38 +0200

    Django Templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
 	print(args, kwargs)
 	print(request.user)
-	return HttpResponse("<h1>Hello Django</h1>")
+	return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-	return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+	return render(request, "contact.html", {})
