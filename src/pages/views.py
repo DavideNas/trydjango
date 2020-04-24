71b7ac2208870a5aea9ca049f91297cab9beaabd
DavideNas <d.naselli@gmail.com>
2020-04-24 21:11:46 +0200

    Rendering Context In A Template

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
-	print(args, kwargs)
-	print(request.user)
 	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs0):
+	context = {
+		"my_text": "This is about us",
+		"my_number": 123,
+		"my_list": [123, 4242,12313]
+	}
+	return render(request, "about.html", context)
