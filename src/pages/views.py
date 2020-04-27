11aaa24aaed20c6952b96d813fab3708e10c4395
DavideNas <d.naselli@gmail.com>
2020-04-27 09:37:32 +0200

    Template Tags And Filters

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,6 +13,7 @@ def about_view(request, *args, **kwargs0):
 	context = {
 		"my_text": "This is about us",
 		"this_is_true": True,
+		"my_html":"<h3>Hello <b>World</b> !</h3>",
 		"my_number": 123,
 		"my_list": [123, 4242,"Abc",12313]
 	}
