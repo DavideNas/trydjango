ecbac72ab4d09c6a76e44ee6d7fd7938e3dba18d
DavideNas <d.naselli@gmail.com>
2020-04-27 10:46:11 +0200

    Render Data From The Database With A Model

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+def product_detail_view(request, *args, **kwargs):
+	
+	obj = Product.objects.get(id=1)
+
+	'''
+	context = {
+		'title': obj.title,
+		'description': obj.description,
+		'price': obj.price
+	}
+	'''
+	context = {
+		'spezia': obj
+	}
+
+	return render(request, "product/detail.html", context)
