e7f5296a609167b8dcaed68ac5511d486a06920f
mintpc <d.naselli@gmail.com>
2020-05-18 09:06:39 +0200

    View of a List of Database Objects

--- src/products/views.py
+++ src/products/views.py
@@ -5,20 +5,12 @@ from .forms import ProductForm, RawProductForm
 from .models import Product
 # Create your views here.
 
-#def product_create_view(request):
-#	my_form = RawProductForm()
-#	if request.method == "POST":
-#		my_form = RawProductForm(request.POST)
-#		if my_form.is_valid():
-#			# now data is good
-#			print(my_form.cleaned_data)
-#			Product.objects.create(**my_form.cleaned_data)
-#		else:
-#			print(my_form.errors)
-#	context = {
-#		"form": my_form
-#	}
-#	return render(request, "products/product_create.html", context)
+def product_list_view(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset
+	}
+	return render(request, "products/product_list.html", context)
 
 def dynamic_lookup_views(request, id):
 	try:
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 
 from pages.views import home_view, contact_view, about_view
 
-from products.views import product_detail_view, product_create_view, render_initial_data, dynamic_lookup_views, product_delete_view
+from products.views import product_detail_view, product_create_view, render_initial_data, dynamic_lookup_views, product_delete_view, product_list_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -31,4 +31,5 @@ urlpatterns = [
     path('initial/', render_initial_data),
     path('products/<int:id>/', dynamic_lookup_views, name='product'),
     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
+    path('products/list/', product_list_view, name='product-list'),
 ]
